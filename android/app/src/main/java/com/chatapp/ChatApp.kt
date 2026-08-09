eefaca09f928d2e13a4d866c16939fdc8f1f00fe
Chat App Dev <chatapp@example.com>
2026-08-09 13:11:30 +0530

    Initialize MessageCache on app start to fix startup crash

--- android/app/src/main/java/com/chatapp/ChatApp.kt
+++ android/app/src/main/java/com/chatapp/ChatApp.kt
@@ -2,6 +2,7 @@ package com.chatapp
 
 import android.app.Application
 import com.chatapp.data.ApiClient
+import com.chatapp.data.MessageCache
 import com.chatapp.data.SocketManager
 import com.chatapp.data.Session
 
@@ -15,6 +16,7 @@ class ChatApp : Application() {
     override fun onCreate() {
         super.onCreate()
         Session.init(this)
+        MessageCache.init(this)
         ApiClient.configure(Session.serverBase())
         if (Session.current.token != null) {
             SocketManager.connect(Session.current.token!!)
